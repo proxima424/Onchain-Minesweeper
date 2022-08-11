cbbe3bbe3011658a11afcaeafba3d6c34265e19f
etikshajain <85029815+etikshajain@users.noreply.github.com>
2022-08-11 22:59:02 +0530

    Update factory.sol
    Check out

--- contracts/factory.sol
+++ contracts/factory.sol
@@ -12,42 +12,93 @@ pragma solidity ^0.8.10;
 
 contract factory{
     //global variables
-
-    address playerA,playerB;
-    uint256 gameState = 0;
+    address playerA;
+    address playerB;
+    bool gameStarted = false;
+    uint turn = 0; //0->game not started, 1-> playera, 2->playerb, 3->gameover
     mapping(address => bool) gameExists;
     mapping(address => uint256) amountStaked;
-
-
+    mapping(uint => address) gameState; //1 to 25
+    uint bomb_tile = 0;
 
     //events
-    event gameCreated( address indexed player1, address indexed amountA  );
+    event gameCreated( address indexed player, uint amount );
+    event gameOver( address indexed player); //player lost
+    event next_player_turn(address player); //turn of player
+    event game_started();
 
-    constructor( ){
+    constructor(){
         playerA = msg.sender;
-        gameState = 
+        gameStarted = false;
+        bomb_tile = returnRandomNumber();
     }
 
-    function createNewGame payable public (){
-        // check the gameExists mapping
-        // set the amountStaked mapping
-        // emit gameCreated event
+    function createNewGame() payable public {
+        require(playerA != address(0));
+        gameStarted = false;
+        gameExists[playerA] =  true;
+        amountStaked[playerA] = msg.value;
+        emit gameCreated(playerA, amountStaked[playerA]);
     }
-
-     
-    function joinGame( address _playerA ){
-        //This function will take address of the player who created the game.
-        //This new player will be able to join this game only if he commits to a amount greater than or equal to what playerA commited
-        require( msg.value => amountStaked[address]);
-        // A mapping to begin the game maybe?
+ 
+    function joinGame( address _playerB ) payable public {
+        playerB = _playerB;
+        require(msg.value >= amountStaked[playerA]);
+        gameExists[playerB] =  true;
+        amountStaked[playerB] = msg.value;
+        emit gameCreated(playerB, amountStaked[playerB]);
     } 
-    
-    function returnRandomNumber( ) returns(uint256){
-        //////
+
+    function start_game() public {
+        require(playerA != address(0));
+        require(playerB != address(0));
+        gameStarted = true;
+        emit game_started();
     }
 
-
+    function get_player_id(address player) public view returns (uint){
+        require(gameStarted == true);
+        require(turn != 0 && turn != 3);
+        if(player == playerA) {
+            return 1;
+        }
+        else{
+            return 2;
+        }
+    }
     
+    function returnRandomNumber() public returns(uint256){
+        return 4;
+    }
+
+    function updateGameState() public returns(uint) {
+        if(gameState[bomb_tile] != address(0)) {
+            return 3;
+        }
+        else if(turn == 1){
+            emit next_player_turn(playerB);
+            return 2;
+        }
+        else{
+            emit next_player_turn(playerA);
+            return 1;
+        }
+    }
+
+    function move(uint pos) public {
+        require(gameStarted == true, "Wait for player 2 to join.");
+        require(msg.sender == playerA || msg.sender == playerB, "You are not a participant.");
+        require(turn!=0 && turn!=3);
+        require(turn == get_player_id(msg.sender));
+        require(pos>=1 && pos<=25);
+        require(gameState[pos] == address(0), "This tile is already used");
+        gameState[pos] = msg.sender;
+        turn = updateGameState();
+        if(turn == 3){
+            emit gameOver(msg.sender);
+            //transfer money
+            //stop game
+        }
+    } 
 
 }
-
