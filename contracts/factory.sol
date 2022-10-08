042e063dbb09d42b6ab7f9d1a682e4d04bb199b4
Darpit Rangari <95137001+proxima424@users.noreply.github.com>
2022-10-08 11:01:14 +0530

    implement gameLogic.sol

--- contracts/factory.sol
+++ contracts/factory.sol
@@ -15,46 +15,48 @@
 
 pragma solidity ^0.8.10;
 
+contract Ifactory {
+    function createGame() payable public{}
+    function forfeitGame(){} 
+    function joinGame(){} 
+}
+
 contract factory{
-    //global variables
-    
-    mapping(address => bool) gameExists;
-    mapping(address => bool) gameStarted;
-    mapping(address => address) currentgameAddress;
-   
+    //global state variables
+    mapping(address => gameData) gameInfo;          //Every player will be assigned this gameData struct
+    mapping(address=>uint256) amountStaked;
+    address immutable gameInstance;
 
-
-    //events
+    //Events
     event gameCreated( address indexed player1, address indexed amountA  );
 
+    //Structs
+    struct gameData{
+        address gameAddress; //160bits
+        uint96  gameState; //96bits  
+
+    }
+    // uint96 gameState gives two separate information bit packed together.
+    // It tells whether game exists
+    // It tells whether game has started
+
+   
+
     constructor(){
-       
+       gameInstance = ;    
     }
 
     function createGame() payable public{
-        require(msg.value > 0 );                                 // checks if player has associated some money
-        require(gameExists[msg.sender] == false);                // check if player is involved in another game 
-        gameExists[playerA] =  true;                             
-        amountStaked[playerA] = msg.value;                       // set the amountStaked mapping
-        emit gameCreated(playerA, amountStaked[playerA]);        // emit gameCreated event
+        require(msg.value > 0 );                                 // checks if player has given some money
+        require(gameInfo[msg.sender].gameStarted == f);          // check if player is involved in another game                        
+        amountStaked[msg.sender] = msg.value;                    // set the amountStaked mapping
+        emit gameCreated(msg.sender, amountStaked[msg.sender]);        // emit gameCreated event
 
         // Implement creation of a new instance of the gameLogic contract.
     }
 
-    function forfeitGame(){
-        // A player can end the game by forfeiting at any time
-        // Amount staked will go to the other player. Hahahahahhahaha
-    } 
-
-    
-    function joinGame( address _playerA ){
-        //This function will take address of the player who created the game.
-        //This new player will be able to join this game only if he commits to a amount greater than or equal to what playerA commited
-        require( msg.value => amountStaked[address]);
-        require( gameStarted[_playerA] == false );
-        
-        // call the gameLogic contract associated with playerA and associate the msg.sender with it
-    } 
+    function forfeitGame(){} 
+    function joinGame(){} 
     
     
 }
